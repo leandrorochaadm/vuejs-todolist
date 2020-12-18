c6d1b762c7259eb6544ecbc15cd260cf110be54a
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-18 13:09:45 -0400

    formantando o botao deletar tarefa

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="task"
     :class="stateClass">
+    <span class="close">x</span>
       <p>{{task.name}}</p>
   </div>
 </template>
@@ -23,6 +24,7 @@ export default {
 
 <style>
     .task{
+        position: relative;
         box-sizing: border-box;
         width: 350px;
         height: 150px;
@@ -49,4 +51,24 @@ export default {
         text-decoration: line-through;
     }
 
+    .pending .close{
+        background-color: #b73229;
+    }
+
+    .done .close{
+        background-color: #0A8F08;
+    }
+
+    .close{
+        position: absolute;
+        right: 10px;
+        top: 10px ;
+        font-size: 0.9rem;
+        font-weight: 900;
+        height: 20px;
+        width: 20px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+    }
 </style>
